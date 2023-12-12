ea67ceac65de149be1bdb44d4812c4825852accb
Meoowka <sasha.medvedev2017@mail.ru>
2023-12-12 17:42:26 +0300

    h

---
+++ Client__ftp/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Client__ftp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            toolStrip1 = new ToolStrip();
+            toolStripLabel1 = new ToolStripLabel();
+            Host_text = new ToolStripTextBox();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripLabel2 = new ToolStripLabel();
+            log_text = new ToolStripTextBox();
+            toolStripSeparator2 = new ToolStripSeparator();
+            toolStripLabel3 = new ToolStripLabel();
+            pass_text = new ToolStripTextBox();
+            toolStripSeparator3 = new ToolStripSeparator();
+            toolStripButton1 = new ToolStripButton();
+            toolStripSeparator4 = new ToolStripSeparator();
+            toolStripButton2 = new ToolStripButton();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.Left;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1, Host_text, toolStripSeparator1, toolStripLabel2, log_text, toolStripSeparator2, toolStripLabel3, pass_text, toolStripSeparator3, toolStripButton1, toolStripSeparator4, toolStripButton2 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(101, 450);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(98, 15);
+            toolStripLabel1.Text = "Host";
+            // 
+            // Host_text
+            // 
+            Host_text.Name = "Host_text";
+            Host_text.Size = new Size(96, 23);
+            Host_text.Text = "127.0.0.1";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(98, 6);
+            // 
+            // toolStripLabel2
+            // 
+            toolStripLabel2.Name = "toolStripLabel2";
+            toolStripLabel2.Size = new Size(98, 15);
+            toolStripLabel2.Text = "user";
+            // 
+            // log_text
+            // 
+            log_text.Name = "log_text";
+            log_text.Size = new Size(96, 23);
+            log_text.Text = "Test_1";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(98, 6);
+            // 
+            // toolStripLabel3
+            // 
+            toolStripLabel3.Name = "toolStripLabel3";
+            toolStripLabel3.Size = new Size(98, 15);
+            toolStripLabel3.Text = "password";
+            // 
+            // pass_text
+            // 
+            pass_text.Name = "pass_text";
+            pass_text.Size = new Size(96, 23);
+            pass_text.Text = "123123123zx";
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(98, 6);
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = Properties.Resources.connect;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(98, 20);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += toolStripButton1_Click;
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(98, 6);
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = Properties.Resources.dis;
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(98, 20);
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(106, 0);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(326, 206);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.TabStop = false;
+            richTextBox1.Text = "";
+            richTextBox1.WordWrap = false;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(0, 221);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.ReadOnly = true;
+            richTextBox2.Size = new Size(523, 206);
+            richTextBox2.TabIndex = 2;
+            richTextBox2.TabStop = false;
+            richTextBox2.Text = "";
+            richTextBox2.WordWrap = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(793, 450);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(toolStrip1);
+            Name = "Form1";
+            Text = "Form1";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripLabel toolStripLabel1;
+        private ToolStripTextBox Host_text;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripLabel toolStripLabel2;
+        private ToolStripTextBox log_text;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripLabel toolStripLabel3;
+        private ToolStripTextBox pass_text;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripButton toolStripButton1;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripButton toolStripButton2;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+    }
+}
---
+++ Client__ftp/Form1.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP;
+using Microsoft.VisualBasic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Client__ftp
+{
+    public partial class Form1 : Form
+    {
+        FtpClient client = new FtpClient("", "", "");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Connect(string host, NetworkCredential credential)
+        {
+            client!.Host = host;
+            client.Credentials = credential;
+            client.Connect();
+        }
+
+        public void Disconnect()
+        {
+            client!.Disconnect();
+        }
+
+
+
+        public void ShowLog()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + client.Host);
+
+            request.KeepAlive = false;
+            request.Method = WebRequestMethods.Ftp.PrintWorkingDirectory;
+            request.Credentials = new NetworkCredential(client.Credentials.UserName,
+                client.Credentials.Password);
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+
+            string messageBack;
+
+            messageBack = response.StatusDescription;
+            string[] split = messageBack.Split(' ');
+            richTextBox1.AppendText("Client: Connected to " + client.Host + Environment.NewLine);
+            richTextBox1.AppendText("Client: Sending command \"PWD\"" + Environment.NewLine);
+            richTextBox1.AppendText(string.Format("Server: {0}", response.StatusDescription));
+
+            response.Close();
+
+            richTextBox1.AppendText("Client: Sending command \"LIST\"" + Environment.NewLine);
+        }
+
+
+        public void ShowDir()
+        {
+            var path = Directory.GetCurrentDirectory();
+            var listing = client.GetListing(FtpListOption.NoPath);
+
+            foreach (var item in listing)
+            {
+                switch (item.Type)
+                {
+
+                    case FtpObjectType.Directory:
+
+                        richTextBox2.AppendText("Directory!  " + item.FullName + Environment.NewLine);
+                        richTextBox2.AppendText("Modified date:  " + client.GetModifiedTime(item.FullName) + Environment.NewLine);
+
+                        break;
+
+                    case FtpObjectType.File:
+
+                        richTextBox2.AppendText("File!  " + item.FullName);
+                        richTextBox2.AppendText("File size:  " + client.GetFileSize(item.FullName) + Environment.NewLine);
+                        richTextBox2.AppendText("Modified date:  " + client.GetModifiedTime(item.FullName) + Environment.NewLine);
+                        richTextBox2.AppendText("Chmod:  " + client.GetChmod(item.FullName) + Environment.NewLine);
+
+                        break;
+
+                    case FtpObjectType.Link:
+                        break;
+                }
+            }
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Connect(Host_text.Text, new NetworkCredential(log_text.Text, pass_text.Text));
+            ShowLog();
+            ShowDir();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                Disconnect();
+                richTextBox1.AppendText("Client log out" + Environment.NewLine);
+            }
+
+        }
+
+
+    }
+}
